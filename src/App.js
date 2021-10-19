afb90e1364d88a6e1a74c5e9f065b0b6875a1fb8
Stefp27 <stefania.p27@gmail.com>
2021-10-19 19:12:23 +0200

    Moved project from Codesanbox to VS Code
    - Moved the initial Codesanbox code in VS Code without many changes
    - Will develop this in the next homework.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import Weather from "./Weather"
+import SearchEngine from "./SearchEngine";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1>Hello World</h1>
-       <Weather />
+       <h1>Weather Search Engine</h1>
+       <SearchEngine />
       </header>
     </div>
   );
--- src/Weather.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import './App.css';
-
-export default function Weather () {
-    return (
-        <h2>Hola from Stef!</h2>
-    )
-}
