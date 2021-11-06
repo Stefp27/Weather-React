c102dd00151f94394a861c123cfe4127196ed97c
Stefp27 <stefania.p27@gmail.com>
2021-11-06 13:46:29 +0100

    Added GitHub link

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App"> Weather App </div>
+    <div className="App"> Weather App 
+    <footer> This project is coded by Stefania Popescu and is open-sourced on <a href="https://github.com/Stefp27/Weather-React/tree/master" target="_blank">GitHub</a>
+
+
+
+    </footer>
+    
+    </div>
   );
 }
