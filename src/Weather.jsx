1e4a83777ebe547727f4ec06dcbd60e1b4b8580c
Stefp27 <stefania.p27@gmail.com>
2022-03-31 22:45:41 +0200

    Moved API key info

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -23,7 +23,7 @@ export default function Weather(props) {
   }
 
   function search() {
-    const apiKey = "ba0f360e4ade3504405f1836416cb716";
+    const apiKey = `${process.env.REACT_APP_WEATHER_API}`;
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
       axios.get(apiUrl).then(handleResponse);
   }
